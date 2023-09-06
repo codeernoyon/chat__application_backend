c83f88d00ee7a7ee00d4d2253f506646f6dc126c
Noyon Islam <82902682+codeernoyon@users.noreply.github.com>
2023-09-06 16:18:27 +0600

    get all user router

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -5,57 +5,60 @@ const ErrorHandler = require("../utils/erroeHandler");
 const token = require("../utils/token");
 
 // ------- user save on database ------ //
-const saveUserInDatabase = catchAsyncError(async (req, res, next) => {
+const saveUserInDatabase = async (req, res, next) => {
   const { name, email, imageUrl } = req.body;
+  try {
+    // find user from database
+    const user = await User.findOne({ email });
 
-  // find user from database
-  const user = await User.findOne({ email });
+    // if user already save on database return from hare
+    if (user) {
+      // create a token for user
+      const userToken = token(user);
 
-  // if user already save on database return from hare
-  if (user) {
+      // send a response with user data and cookie
+      res
+        .header(
+          "Access-Control-Allow-Headers",
+          "x-access-token, Origin, Content-Type, Accept"
+        )
+        .cookie("userToken", userToken, {
+          httpOnly: true,
+          // secure: true,
+        })
+        .status(200)
+        .json({
+          message: "Welcome to your world",
+          token: userToken,
+          user,
+        });
+    }
+
+    // create a new user in database
+    const newUser = new User({
+      name,
+      email,
+      imageUrl,
+    });
+    //  save user in database
+    await newUser.save();
     // create a token for user
-    const userToken = token(user);
+    const userToken = token(newUser);
 
-    // send a response with user data and cookie
     res
-      .header(
-        "Access-Control-Allow-Headers",
-        "x-access-token, Origin, Content-Type, Accept"
-      )
       .cookie("userToken", userToken, {
         httpOnly: true,
-        // secure: true,
       })
       .status(200)
       .json({
-        message: "Welcome to your world",
+        message: "User Create Successful",
         token: userToken,
-        user,
+        user: newUser?._doc,
       });
+  } catch (error) {
+    next(error);
   }
-
-  // create a new user in database
-  const newUser = new User({
-    name,
-    email,
-    imageUrl,
-  });
-  //  save user in database
-  await newUser.save();
-  // create a token for user
-  const userToken = token(newUser);
-
-  res
-    .cookie("userToken", userToken, {
-      httpOnly: true,
-    })
-    .status(200)
-    .json({
-      message: "User Create Successful",
-      token: userToken,
-      user: newUser?._doc,
-    });
-});
+};
 // ------- user image update on database ------ //
 const updateUserInfo = catchAsyncError(async (req, res, next) => {
   const { name, email, imageUrl, description } = req.body;
@@ -76,4 +79,26 @@ const updateUserInfo = catchAsyncError(async (req, res, next) => {
   });
 });
 
-module.exports = { saveUserInDatabase, updateUserInfo };
+// get all user from data base
+const getAllUser = catchAsyncError(async (req, res, next) => {
+  const { email } = req.body;
+  try {
+    // find user from database
+    const user = await User.findOne({ email });
+
+    // if don't match user save user in database through error
+    if (!user) {
+      return next(new ErrorHandler("User dose't exist"));
+    }
+
+    User.find({}).then(function (users) {
+      res.status(200).json({
+        allUsers: users,
+      });
+    });
+  } catch (error) {
+    next(error.message);
+  }
+});
+
+module.exports = { saveUserInDatabase, updateUserInfo, getAllUser };
--- routes/AuthRoutes.js
+++ routes/AuthRoutes.js
@@ -3,11 +3,13 @@ const express = require("express");
 const {
   saveUserInDatabase,
   updateUserInfo,
+  getAllUser,
 } = require("../controllers/AuthController");
 const AuthRoute = express.Router();
 
 // --------- Routers ------- //
 AuthRoute.post("/user", saveUserInDatabase);
 AuthRoute.put("/updateUserInfo", updateUserInfo);
+AuthRoute.post("/allUsers", getAllUser);
 
 module.exports = { AuthRoute };
