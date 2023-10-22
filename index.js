174018ff1af28ea5eefa47300f13b7fc932f5dc6
Noyon Islam <82902682+codeernoyon@users.noreply.github.com>
2023-10-22 21:07:13 +0600

    update index.js file

--- index.js
+++ index.js
@@ -15,12 +15,6 @@ const corsOptions = {
   credentials: true,
   optionSuccessStatus: 200,
 };
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
 // -------- app create for use application -------- //
 const app = express();
 dotenv.config();
