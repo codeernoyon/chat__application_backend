530afd3a17bf6c0459ed23da36ebab094e353d4b
Noyon Islam <82902682+codeernoyon@users.noreply.github.com>
2023-10-22 21:45:44 +0600

    update index.js file

--- index.js
+++ index.js
@@ -41,6 +41,14 @@ app.use(cookieParser());
 
 // -------- Routing ----------- //
 app.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Max-Age", "1800");
+  res.setHeader("Access-Control-Allow-Headers", "content-type");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
+  );
   res.status(200).json({
     message: "Welcome",
   });
